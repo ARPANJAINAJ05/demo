fa026a428d49d23b02c9433d68e05c6c6de167de
arpan <jainarpannit@gmail.com>
2022-07-03 14:53:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Demo deployment on netlify
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
